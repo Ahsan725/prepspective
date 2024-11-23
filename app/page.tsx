ecb8236b217eb460566df42cc9f2f35f34a82d08
Ahsan <sbaseer25@gmail.com>
2024-11-23 00:12:14 -0500

    added a bento box

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import CustomHero from '@/components/ui/customhero';
 import FAQSection from '@/components/ui/customfaq';
 import { FeaturesSection } from "@/components/ui/customfeature2";
 import Pitch from "@/components/ui/pitch"
+import Bento from "@/components/ui/bento"
 
 
 export default function HomePage() {
   return (
     <main>
       <CustomHero />
+      <Bento/>
       <Pitch />
       {/* <CustomFeature/> */}
       <FeaturesSection/>
