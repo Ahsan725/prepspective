c692e1f958977664fe88c9f18993bbecd4f1dc64
Ahsan <sbaseer25@gmail.com>
2024-11-25 15:23:22 -0500

    some new changes

--- app/interview/results/[id]/page.tsx
+++ app/interview/results/[id]/page.tsx
@@ -201,8 +201,8 @@ const DetailView: React.FC = () => {
                 onClick={() => setActiveTab(key)}
                 className={`inline-flex items-center justify-center p-4 border-b-2 ${
                   activeTab === key
-                    ? 'text-white bg-indigo-700 border-indigo-600 rounded-t-lg px-2 py-2 dark:text-indigo-500 dark:border-indigo-500'
-                    : 'border-transparent px-2 py-2 hover:text-indigo-700 hover:border-indigo-700 dark:hover:text-gray-300'
+                    ? 'text-white bg-indigo-700 font-semibold border-indigo-600 rounded-t-lg px-2 py-2 dark:text-indigo-500 dark:border-indigo-500'
+                    : 'border-transparent px-2 py-2 font-semibold hover:text-indigo-700 hover:border-indigo-700 dark:hover:text-gray-300'
                 }`}
               >
                 {icon}
