7ce9ed7780b4ad3f04855764e31f049a1db478af
Ahsan <sbaseer25@gmail.com>
2024-11-29 04:42:01 -0500

    added a new section for familair

--- components/ui/familiar.tsx
+++ components/ui/familiar.tsx
@@ -95,7 +95,10 @@ const Testimonial14 = () => {
                     <h3 className="text-2xl font-medium text-gray-600 mb-4">
                       {testimonial.heading}
                     </h3>
-                    <p className="mx-auto text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-lg dark:text-gray-400">
+                          <p className="mx-auto text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-lg dark:text-gray-400">
+                          <svg className="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+    </svg>
                       {testimonial.text}
                     </p>
                   </div>
