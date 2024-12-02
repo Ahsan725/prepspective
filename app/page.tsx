c0bf62cd76019bc79d02b5fcef01acd427d3e796
Ahsan <sbaseer25@gmail.com>
2024-12-02 03:03:36 -0500

    added vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,9 @@ import FAQSection from '@/components/ui/customfaq';
 import { FeaturesSection } from "@/components/ui/customfeature2";
 import Familiar from "@/components/ui/familiar";
 import Bento from "@/components/ui/bento"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const scenarios = [
-  {
-    title: "The Reddit Rabbit Hole",
-    text: "You finally land an interview at your dream company, but panic sets in—what do they even ask?",
-  },
-  {
-    title: "Glassdoor Guesswork",
-    text: "Searching for 'Software Engineer Intern interviews' on Glassdoor and reading about marketing intern interviews instead?",
-  },
-  {
-    title: "The LeetCode Premium Gamble",
-    text: "You paid for LeetCode Premium, expecting gold, but instead found Meta tagged with dynamic programming.",
-  },
-];
+
 export default function HomePage() {
   return (
     <main>
@@ -27,6 +15,7 @@ export default function HomePage() {
       <Familiar />
       <FeaturesSection/>
       < FAQSection />
+      <SpeedInsights />
     </main>
   );
 }
