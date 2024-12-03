c403838c4737cfb578447b17a757ee0b84db9a18
Ahsan <sbaseer25@gmail.com>
2024-12-02 22:13:58 -0500

    clerk error

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
-
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
-})
+export default clerkMiddleware();
 
 export const config = {
-    matcher: [
-      // Allow public access to the root route "/"
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Protect API routes and other specified routes
-      '/(api|trpc)(.*)',
-      '/interview/search',
-      // Exclude the root route "/"
-      '!/',
-    ],
-  };
-  
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/interview/search',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
