a79b955508167178eb109bc2d0c5fb08c95a3eaf
Ahsan <sbaseer25@gmail.com>
2024-12-02 19:31:49 -0500

    removed a few files that were not needed if something breaks revert to stable build

--- components/ui/customhero.tsx
+++ components/ui/customhero.tsx
@@ -138,7 +138,7 @@ const CustomHero = () => {
 
         {/* Waitlist Count */}
         <p className="whitespace-pre-wrap tracking-tighter">
-          <NumberTicker value={count + 193} className="text-xl text-indigo-600 font-extrabold" /> people have already joined the waitlist!
+          <NumberTicker value={count + 217} className="text-xl text-indigo-600 font-extrabold" /> people have already joined the waitlist!
         </p>
 
         {/* Waitlist Form */}
