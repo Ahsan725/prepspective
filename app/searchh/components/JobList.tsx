3e442d8eb47d71319ca2450f53f97de74e34a257
Ahsan <sbaseer25@gmail.com>
2024-12-09 08:19:16 -0500

    changed badge colors

--- app/searchh/components/JobList.tsx
+++ app/searchh/components/JobList.tsx
@@ -66,64 +66,64 @@ const JobList: React.FC<JobListProps> = ({
 
                 {/* Display All Badges */}
                 <div className="flex flex-wrap gap-1">
-                  {result.level && (
-                    <Badge className="bg-indigo-500 text-white text-[9px]">
-                      {result.level}
-                    </Badge>
-                  )}
-                  {result.jobOffer === true && (
-                    <Badge className="bg-green-100 text-green-800 text-[9px]">Offer</Badge>
-                  )}
-                  {result.jobOffer === false && (
-                    <Badge className="bg-red-100 text-red-800 text-[9px]">No Offer</Badge>
-                  )}
-                  {result.questions.some((q) => q.type.toLowerCase() === 'behavioral') && (
-                    <Badge className="bg-yellow-100 text-yellow-800 text-[9px]">
-                      Behavioral
-                    </Badge>
-                  )}
-                  {result.questions.some((q) => q.type.toLowerCase() === 'technical') && (
-                    <Badge className="bg-purple-100 text-purple-800 text-[9px]">
-                      Technical
-                    </Badge>
-                  )}
-                  {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('system design')
-                  ) && (
-                    <Badge className="bg-indigo-100 text-indigo-800 text-[9px]">
-                      System Design
-                    </Badge>
-                  )}
-                  {result.questions.some((q) => q.leetcodeLink) && (
-                    <Badge className="bg-teal-100 text-teal-800 text-[9px]">LeetCode</Badge>
-                  )}
-                  {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('pre screen')
-                  ) && (
-                    <Badge className="bg-cyan-100 text-cyan-800 text-[9px]">Pre Screen</Badge>
-                  )}
-                  {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('oa')
-                  ) && (
-                    <Badge className="bg-pink-100 text-pink-800 text-[9px]">OA</Badge>
-                    )}
-                                    {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('hr')
-                  ) && (
-                    <Badge className="bg-blue-300 text-blue-900 text-[9px]">HR</Badge>
-                    )}
-                                    {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('onsite')
-                  ) && (
-                    <Badge className="bg-pink-300 text-pink-800 text-[9px]">Onsite</Badge>
-                    )}
-                                                      {result.rounds.some((round) =>
-                    round.roundType.toLowerCase().includes('team matching')
-                  ) && (
-                    <Badge className="bg-teal-300 text-teal-800 text-[9px]">Team Match</Badge>
-                  )}
-                  
-                </div>
+  {result.level && (
+    <Badge className="bg-gradient-to-r from-indigo-800 to-indigo-500 text-white text-[9px]">
+      {result.level}
+    </Badge>
+  )}
+  {result.jobOffer === true && (
+    <Badge className="bg-green-100 text-green-800 text-[9px]">Offer</Badge>
+  )}
+  {result.jobOffer === false && (
+    <Badge className="bg-red-100 text-red-800 text-[9px]">No Offer</Badge>
+  )}
+  {result.questions.some((q) => q.type.toLowerCase() === 'behavioral') && (
+    <Badge className="bg-yellow-100 text-yellow-800 text-[9px]">
+      Behavioral
+    </Badge>
+  )}
+  {result.questions.some((q) => q.type.toLowerCase() === 'technical') && (
+    <Badge className="bg-purple-100 text-purple-800 text-[9px]">
+      Technical
+    </Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('system design')
+  ) && (
+    <Badge className="bg-blue-100 text-blue-800 text-[9px]">
+      System Design
+    </Badge>
+  )}
+  {result.questions.some((q) => q.leetcodeLink) && (
+    <Badge className="bg-teal-100 text-teal-800 text-[9px]">LeetCode</Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('pre screen')
+  ) && (
+    <Badge className="bg-cyan-100 text-cyan-800 text-[9px]">Pre Screen</Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('oa')
+  ) && (
+    <Badge className="bg-pink-100 text-pink-800 text-[9px]">OA</Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('hr')
+  ) && (
+    <Badge className="bg-orange-100 text-orange-800 text-[9px]">HR</Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('onsite')
+  ) && (
+    <Badge className="bg-lime-100 text-lime-800 text-[9px]">Onsite</Badge>
+  )}
+  {result.rounds.some((round) =>
+    round.roundType.toLowerCase().includes('team matching')
+  ) && (
+    <Badge className="bg-emerald-100 text-emerald-800 text-[9px]">Team Match</Badge>
+  )}
+</div>
+
               </li>
             );
           })
