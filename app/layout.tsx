159ea99dbc8685e2ce59efff79c5235a6b83f7dc
Ahsan <sbaseer25@gmail.com>
2025-01-06 23:35:18 -0500

    Production release

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimary: 'bg-indigo-700 text-primary-foreground hover:bg-indigo-700/90 relative overflow-hidden rounded-md px-5 py-2.5 text-white duration-300 shadow-xl hover:shadow-xl [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-102 active:scale-y-97 bg-gradient-to-r from-indigo-800 to-indigo-500 text-sm',
+      },
+    }}>
       {/* Wrap the entire app with ClerkProvider */}
       <html lang="en">
         <head>
