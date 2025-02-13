45c88ef50e6fac74c18d6fd5cb343f1e75b08603
Ahsan <sbaseer25@gmail.com>
2025-02-13 00:44:35 -0500

    disaster

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: '../.env' }); // Load environment variables
 
 export default defineConfig({
   schema: './db/schema.ts', // Path to your schema
-  out: './supabase/migrations', // Migration output path
+  out: './supabase/migrations', // Migration output path there is a fix required for this to work
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!, 
