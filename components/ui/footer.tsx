c7d0f9668dde85038cfb656e216733f7f15978da
Ahsan <sbaseer25@gmail.com>
2025-03-22 03:49:58 -0400

    about and navbar update

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -37,19 +37,19 @@ const Footer = () => {
     <section className="relative py-32 bg-white overflow-hidden pb-4">
       <div className="container mx-auto px-4">
         <footer>
-          <div className="flex flex-col items-start justify-between gap-10 text-center lg:flex-row lg:text-left">
+          <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between gap-10 text-center lg:text-left">
             {/* Logo and Description */}
-            <div className="flex w-full max-w-96 shrink flex-col items-center justify-between gap-6 lg:items-start">
-              <div className="flex items-center gap-2 lg:justify-start">
+            <div className="flex w-full max-w-96 shrink flex-col items-center lg:items-start justify-between gap-6">
+              <div className="flex items-center gap-2">
                 <span className="text-2xl font-extrabold text-indigo-700">
                   {'{P}rep'}
                   <span className="font-bold text-indigo-700">Spective</span>
                 </span>
               </div>
-              <p className="text-sm text-gray-600">
+              <p className="text-sm text-gray-600 max-w-sm">
                 Built to help you land your next role — with tools, resources, and people who’ve done it before.
               </p>
-              <ul className="flex items-center space-x-6 text-indigo-700">
+              <ul className="flex justify-center lg:justify-start items-center space-x-6 text-indigo-700">
                 <li className="hover:text-indigo-800">
                   <a href="#"><FaInstagram className="size-6" /></a>
                 </li>
@@ -66,7 +66,7 @@ const Footer = () => {
             </div>
 
             {/* Link Sections */}
-            <div className="grid grid-cols-3 gap-6 lg:gap-20">
+            <div className="grid grid-cols-3 text-center gap-6 lg:gap-20 w-full max-w-xl">
               {sections.map((section, sectionIdx) => (
                 <div key={sectionIdx}>
                   <h3 className="mb-6 font-bold text-indigo-700">{section.title}</h3>
@@ -83,9 +83,9 @@ const Footer = () => {
           </div>
 
           {/* Bottom bar */}
-          <div className="mt-20 flex flex-col justify-between gap-4 border-t pt-8 text-center text-sm font-medium text-gray-500 lg:flex-row lg:items-center lg:text-left">
+          <div className="mt-20 flex flex-col lg:flex-row justify-between gap-4 border-t pt-8 text-center text-sm font-medium text-gray-500 lg:text-left">
             <p>© 2024 PrepSpective. All rights reserved.</p>
-            <ul className="flex justify-center gap-4 lg:justify-start">
+            <ul className="flex justify-center lg:justify-start gap-4">
               <li className="hover:text-indigo-600">
                 <a href="#">Terms and Conditions</a>
               </li>
