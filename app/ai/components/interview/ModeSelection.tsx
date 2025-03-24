9753a11dc5db615ec91ba4760581de778dcc5e68
Ahsan <sbaseer25@gmail.com>
2025-03-24 06:08:11 -0400

    exp super exp

--- app/ai/components/interview/ModeSelection.tsx
+++ app/ai/components/interview/ModeSelection.tsx
@@ -24,15 +24,61 @@ export const ModeSelection: React.FC<ModeSelectionProps> = ({ onModeSelect }) =>
       transition={{ duration: 0.6, ease: 'easeOut' }}
       className="w-full max-w-4xl mx-auto mb-10"
     >
+       <motion.div
+        className="flex items-center justify-center mb-4"
+        variants={{
+          hidden: { opacity: 0, y: 10, filter: "blur(10px)" },
+          visible: {
+            opacity: 1,
+            y: 0,
+            filter: "blur(0px)",
+            transition: { duration: 0.5 },
+          },
+        }}
+      >
+        <h2 className="inline-block font-extrabold text-xs sm:text-xs md:text-sm lg:text-md text-indigo-700 text-center tracking-wider bg-indigo-200 rounded-md px-2 py-0">
+          AI INTERVIEW
+        </h2>
+      </motion.div>
+
+      <motion.h2
+        className="text-3xl md:text-4xl text-center font-bold mb-4"
+        variants={{
+          hidden: { opacity: 0, y: 10, filter: "blur(10px)" },
+          visible: {
+            opacity: 1,
+            y: 0,
+            filter: "blur(0px)",
+            transition: { duration: 0.5, delay: 0.2 },
+          },
+        }}
+      >
+        Choose Your Mode
+      </motion.h2>
+
+      <motion.h3
+        className="mx-auto text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
+        variants={{
+          hidden: { opacity: 0, y: 10, filter: "blur(10px)" },
+          visible: {
+            opacity: 1,
+            y: 0,
+            filter: "blur(0px)",
+            transition: { duration: 0.5, delay: 0.4 },
+          },
+        }}
+      >
+        Land your dream role faster with AI interview feedback that is insanely accurate, brutally honest, and tailored to make you stand out.
+      </motion.h3>
       <Card className="rounded-xl border-none shadow-none">
-        <CardHeader className="text-center pb-2">
+        {/* <CardHeader className="text-center pb-2">
           <CardTitle className="text-3xl font-bold text-slate-800">
             Choose Your Mode
           </CardTitle>
           <CardDescription className="text-slate-600 text-base mt-1">
             What type of interview would you like to practice?
           </CardDescription>
-        </CardHeader>
+        </CardHeader> */}
 
         <CardContent className="grid grid-cols-1 sm:grid-cols-2 gap-6 px-6 pb-6">
           {/* Technical Mode */}
