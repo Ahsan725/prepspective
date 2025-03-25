24e8c1d0fd7a3016edbaef1f09c53178c6ee40e6
Ahsan <sbaseer25@gmail.com>
2025-03-24 22:32:32 -0400

    minor

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -16,7 +16,7 @@ const sections = [
     title: 'Company',
     links: [
       { name: 'About', href: '/about' },
-      { name: 'Team', href: '#' },
+      { name: 'Team', href: 'https://www.linkedin.com/in/ahsan-/' },
       { name: 'Blog', href: '#' },
       { name: 'Careers', href: '#' },
     ],
